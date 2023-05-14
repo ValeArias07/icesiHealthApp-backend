89e28b02b3893e66dafec9ca906ad9143eae6037
Valentina Arias Parra <54784992+ValeArias07@users.noreply.github.com>
2023-05-14 17:45:31 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner(
   {
-    serverUrl: 'http://20.169.198.136:9000/',
+    serverUrl: 'http://20.228.242.97:9000/',
     token: process.env.SONAR_TOKEN || '',
     options: {}
   },
